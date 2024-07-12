a0c6fde4b9eb951db6828564ed185f66bd981834
Rohansaini1512 <2022kucp1050@iiitkota.ac.in>
2024-07-12 10:56:56 +0530

    Adding checkout failure page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import HomePage from "./Pages/HomePage";
 import Login from "./Pages/Login";
 import NotFound from "./Pages/NotFound";
 import Checkout from "./Pages/Payment/Checkout";
+import CheckoutFailure from "./Pages/Payment/CheckoutFailure";
 import CheckoutSuccess from "./Pages/Payment/CheckoutSuccess";
 import Signup from "./Pages/Signup";
 import EditProfile from "./Pages/User/EditProfile";
@@ -37,7 +38,8 @@ function App() {
       <Route path="/user/profile" element={<Profile />}/>
       <Route path="/user/editprofile" element={<EditProfile/>}/>
       <Route path="/checkout" element={<Checkout />}/> 
-      <Route path="/checkout/success" element={<CheckoutSuccess />}/> 
+      <Route path="/checkout/success" element={<CheckoutSuccess />}/>
+      <Route path="/checkout/fail" element={<CheckoutFailure />}/> 
 
       <Route element={<RequireAuth allowedRoles={["ADMIN"]}/>}>
         <Route path="/course/create" element={<CreateCourse />}/>
