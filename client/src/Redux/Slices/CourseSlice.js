cde94501bc951790b5ba8375d9866addb573d0f0
Rohansaini1512 <2022kucp1050@iiitkota.ac.in>
2025-07-07 12:25:56 +0530

    Adding admin section

--- client/src/Redux/Slices/CourseSlice.js
+++ client/src/Redux/Slices/CourseSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 import axiosInstance from "../../Helpers/axiosInstance";
 
@@ -7,20 +7,35 @@ const initialState = {
     courseData: []
 }
 
-export const getAllCourses = createAsyncThunk("/course/get", async() => {
-    try{
+export const getAllCourses = createAsyncThunk("/course/get", async () => {
+    try {
         const response = axiosInstance.get("/courses");
-        toast.promise(response,{
-            loading: "loading course data ...",
+        toast.promise(response, {
+            loading: "loading course data...",
             success: "Courses loaded successfully",
             error: "Failed to get the courses",
         });
 
         return (await response).data.courses;
-    }catch(error){
+    } catch(error) {
         toast.error(error?.response?.data?.message);
     }
-});
+}); 
+
+export const deleteCourse = createAsyncThunk("/course/delete", async (id) => {
+    try {
+        const response = axiosInstance.delete(`/courses/${id}`);
+        toast.promise(response, {
+            loading: "deleting course ...",
+            success: "Courses deleted successfully",
+            error: "Failed to delete the courses",
+        });
+
+        return (await response).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+}); 
 
 export const createNewCourse = createAsyncThunk("/course/create", async (data) => {
     try {
@@ -35,7 +50,7 @@ export const createNewCourse = createAsyncThunk("/course/create", async (data) =
         toast.promise(response, {
             loading: "Creating new course",
             success: "Course created successfully",
-            error: "Failed to create course2"
+            error: "Failed to create course"
         });
 
         return (await response).data
@@ -50,9 +65,8 @@ const courseSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase(getAllCourses.fulfilled, (state, action)=> {
-            if(action.payload){
-                console.log(action.payload);
+        builder.addCase(getAllCourses.fulfilled, (state, action) => {
+            if(action.payload) {
                 state.courseData = [...action.payload];
             }
         })
